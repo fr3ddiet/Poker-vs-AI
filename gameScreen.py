cc2614be4cdd9b4f4587d08d165255895f91e8a3
fr3ddiet <146208061+fr3ddiet@users.noreply.github.com>
2023-10-29 19:49:42 +0000

    Update gameScreen.py

--- gameScreen.py
+++ gameScreen.py
@@ -8,6 +8,10 @@ displayw = 1000
 displayh = 600
 screen = display.set_mode((displayw,displayh))
 
+blue = (51, 102, 255)
+green = (0, 153, 0)
+white = (255,255,255)
+
 #clock
 clock = time.Clock()
 
@@ -21,24 +25,29 @@ class gameScreen:
         self.__displayh = displayh 
         self.__playerTurn = True # the player goes first, ai second
         self.__pot = 0
-        self.__player1bal = 1000
-        self.__player2bal = 1000
+        self.__player1bal = 500
+        self.__player2bal = 500
 
     def Button(self,x,y,text):
         if x /2 <= self.mouse[0] <= x/2 + 140 and y/2 <= self.mouse[1] < y/2 + 40:
             draw.rect(screen, (100,100,100), [x /2, y/2, 140, 50])
+            draw.rect(screen, white, [x /2, y/2, 140, 50],2)
+
         else:
             draw.rect(screen, (200,200,200), [x /2, y/2, 140, 50])
+            draw.rect(screen, white, [x /2, y/2, 140, 50],2)
 
-        screen.blit(self.__font.render(text, True, (255,255,255)), (x / 2 + 20, y / 2 + 10))
+        screen.blit(self.__font.render(text, True, white), (x / 2 + 20, y / 2 + 10))
 
 
     def handleCall(self):
-        self.__bet = (1010 - self.__player2bal)
-        
-        self.__pot += self.__bet
-        self.__player1bal -= self.__bet
-        print("Call")
+        self.__bet = (510 - self.__player2bal)
+        if self.__player1bal >= self.__bet:
+            self.__pot += self.__bet
+            self.__player1bal -= self.__bet
+            print("Call")
+        else:
+            print("no money")
 
     def handleRaise(self):
         print("Raise")
@@ -49,16 +58,16 @@ class gameScreen:
         pass
 
     def displayBalance(self):
-        screen.blit(self.__font.render(str(self.__player1bal), True, (255,255,255), (0,0,255)),(460,425))
-        screen.blit(self.__font.render(str(self.__player2bal), True, (255,255,255), (0,0,255)),(460,175))
+        screen.blit(self.__font.render(str(self.__player1bal), True, white, green),(460,422))
+        screen.blit(self.__font.render(str(self.__player2bal), True, white, green),(460,150))
 
     def displayCards(self):
-        self.__font = font.Font('freesansbold.ttf',35) # choosing the font and size
+        self.__font = font.Font('freesansbold.ttf',32) # choosing the font and size
 
-        draw.rect(screen, (0,0,0), Rect(425,478,150,80), 2)
-        draw.rect(screen, (0,0,0), Rect(425,28,150,80), 2)
-        screen.blit(self.__font.render(str(deck1.getp1cards()[0]), True, (255,255,255), (0,0,255)),(445,500)) #blit first player card from the deck class
-        screen.blit(self.__font.render(str(deck1.getp1cards()[1]), True, (255,255,255), (0,0,255)),(510,500)) # blit second player card from the deck class
+        draw.rect(screen, white, Rect(425,480,150,80), 2)
+        draw.rect(screen, white, Rect(425,40,150,80), 2)
+        screen.blit(self.__font.render(str(deck1.getp1cards()[0]), True, white, blue),(445,500)) #blit first player card from the deck class
+        screen.blit(self.__font.render(str(deck1.getp1cards()[1]), True, white, blue),(510,500)) # blit second player card from the deck class
 
     def main(self):
         #variables
@@ -66,7 +75,9 @@ class gameScreen:
         round = 0 
 
         while not stopped:
-            screen.fill((0,0,255))
+            screen.fill(blue)
+            draw.rect(screen, green, Rect(150,100,700,400))
+            draw.rect(screen, white, Rect(150,100,700,400),2)
             self.displayCards()
             self.displayBalance()
 
