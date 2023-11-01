501a29f5e5df6c7df519510ad8bbca3d496562fc
fr3ddiet <146208061+fr3ddiet@users.noreply.github.com>
2023-11-01 21:59:41 +0000

    Update cards.py

--- cards.py
+++ cards.py
@@ -10,7 +10,7 @@ class Card:
 	
 class Deck:
 	def __init__(self):
-		self.__values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+		self.__values = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
 		self.__suits = ["H","C","S","D"]
 		self.__count = 0
 	
