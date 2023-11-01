a3c654d981d2c4255e913259475ea45e96978b07
fr3ddiet <146208061+fr3ddiet@users.noreply.github.com>
2023-11-01 16:09:03 +0000

    Update cards.py

--- cards.py
+++ cards.py
@@ -12,29 +12,33 @@ class Deck:
 	def __init__(self):
 		self.__values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 		self.__suits = ["H","C","S","D"]
-		self.__allcards = []
+		self.__count = 0
 	
 	def createDeck(self):
+		self.__allcards = []
 		for suit in self.__suits:
 			for value in self.__values:
 				self.__allcards.append(Card(value,suit))
 				
 		shuffle(self.__allcards)
 	
-	def returnDeck(self):
-		return self.__allcards
+	def increaseCount(self):
+		self.__count +=9
+		if self.__count >= 36:
+			shuffle(self.__allcards)
+			self.__count = 0
 		
 	def getp1cards(self):
-		return [self.__allcards[0].getCard()[0] + self.__allcards[0].getCard()[1] , self.__allcards[1].getCard()[0] + self.__allcards[1].getCard()[1]]
+		return [self.__allcards[self.__count].getCard()[0] + self.__allcards[self.__count].getCard()[1] , self.__allcards[self.__count+1].getCard()[0] + self.__allcards[self.__count+1].getCard()[1]]
 	
 	def getaicards(self):
-		return [self.__allcards[2].getCard()[0] + self.__allcards[2].getCard()[1], self.__allcards[3].getCard()[0] + self.__allcards[3].getCard()[1]]
+		return [self.__allcards[self.__count+2].getCard()[0] + self.__allcards[self.__count+2].getCard()[1], self.__allcards[self.__count+3].getCard()[0] + self.__allcards[self.__count+3].getCard()[1]]
 		
 	def getCommunityCards(self):
 		self.__comcards = []
 		x = 4
 		while x < 9:
-			self.__comcards.append(self.__allcards[x].getCard()[0]+self.__allcards[x].getCard()[1])
+			self.__comcards.append(self.__allcards[self.__count + x].getCard()[0]+self.__allcards[self.__count + x].getCard()[1])
 			x+=1
 		return self.__comcards
 		
