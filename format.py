3c8fc26057da24eb9a56abe63b5714a13499a689
fr3ddiet <146208061+fr3ddiet@users.noreply.github.com>
2023-12-19 10:28:07 +0000

    Update format.py

--- format.py
+++ format.py
@@ -1,36 +1,42 @@
 from cards import *
 
 class Format: 
-    def __init__(self,filename,mode): 
+    def __init__(self,filename,mode): #Sets up constructor with filename and mode it will be opened with
         self.__file = open(filename,mode) 
-        #self.__pairhand = deck1.getAICards() 
-        self.__Cards = "44o"
-        self.__eval = 0  
+        self.__pairhand = deck1.getaicards() # gets the ai's cards from the cards class
+        #self.__allCards 
 
-    def cardRankKey(self): 
-        rank_order = 'AKQJT98765432'
-        return rank_order.index(self.__Cards[0])
+    def getCard(self):
+        return self.__pairhand # returns the ai's 2 cards
 
-    def orderCards(self): 
-        self.__Cards.sort(key=self.cardRankKey)
+    def cardRankKey(self,cards): 
+        rank_order = 'AKQJT98765432'# poker order of cards
+        return [rank_order.index(cards[0])] # returns the order as an index so they can be sorted in ascending order
+
+    def orderCards(self):
+        self.__pairhand.sort(key=self.cardRankKey) # sorts the 2 cards using the key
 
     def formatCards(self): 
-        if self.__pairhand[0][1] == self.__pairhand[1][1]: 
-            add = "s" 
+        if self.__pairhand[0][1] == self.__pairhand[1][1]: # if the suits are equal
+            add = "s" # they are suited
         else: 
-            add = "o" 
-        return self.__pairhand[0][0] + self.__pairhand[1][0] + add 
+            add = "o" # they are not suited
+        return self.__pairhand[0][0] + self.__pairhand[1][0] + add # returns the two values and if they are suited or not
 
     def getEval(self):
-        print("a")
-        self.__file.seek(0)
-        self.__next = False
+        #self.__file.seek(0)
+        self.__next = False #the file lines alternate between the cards and the next line is the evaluation
 
-        for line in self.__file:
-            if line[0:3] == self.__Cards:
-                self.__next = True
+        for line in self.__file: # loops over each line in the file
+            if line[0:3] == self.formatCards(): # if the line is equal to the ai's cards in the correct format
+                self.__next = True # the next line is the evalutation for the card
             elif self.__next:
-                return line
+                return line # returns the evaluation
+
+#Create object of class and define file name and type
 
 f1 = Format('cardeval.txt',"r")
-print(f1.getEval())
+f1.orderCards()   # order the cards so they can be formated
+#print(f1.getCard())
+f1.formatCards() # format the cards
+#print(f1.getEval()) # return the eval of the 2 cards.
